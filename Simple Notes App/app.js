ffbf79c8fa8ea457fc536d428c5983912776d7e8
Saurav Meena <89388429+sauravnareda@users.noreply.github.com>
2021-09-01 17:50:43 +0530

    Create app.js

---
+++ Simple Notes App/app.js
@@ -0,0 +1,65 @@
+const chalk = require('chalk');
+const notes = require('./notes.js');
+const yargs = require('yargs');
+
+yargs.version('1.1.0');
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Description',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title);
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes();
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title);
+    }
+});
+
+yargs.parse();
